a8f97c2ad8737c170f95e60f7cb2db8d3999ea05
Oscar Larios <javier.atoaxaca@gmail.com>
2021-02-24 18:22:14 -0600

    Creando los Archivos de la Capa de Datos (dConexion,dContactData) y de la Capa de Logica (lContact) y Metodos para la Conexion con la DB

--- WFAContact/Datos/dConexion.cs
+++ WFAContact/Datos/dConexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace WFAContact.Datos
 {
     public class dConexion
     {
+        private SqlConnection conection = new SqlConnection(@"Data Source = (localdb)\AT_OAXACA; initial catalog = UsersDB; integrated Security = true;");
+
+        //metodos de apertura y cierre de la Conexion 
+
+
+
     }
 }
